68123d30a7d2df5944acfd5ddab5424c483e06cb
janreerink <janreerink@hotmail.com>
2018-04-21 19:37:58 +0200

    bug fixes dbw2

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -54,12 +54,18 @@ class DBWNode(object):
                                          BrakeCmd, queue_size=1)
 
         # TODO: Create `Controller` object
-        self.controller = Controller(vehicle_mass = vehicle_mass,
-        fuel_capacity=fuel_capacity, brake_deadband=brake_deadband, decel_limit=decel_limit,
-        accel_limit=accel_limit, wheel_radius=wheel_radius, wheel_base=wheel_base, steer_ratio=steer_ratio,
-        max_lat_accel=max_lat_accel, max_steer_angle=max_steer_angle)
+        #self.controller = Controller(
+        #    vehicle_mass = vehicle_mass, fuel_capacity=fuel_capacity, brake_deadband=brake_deadband, decel_limit=decel_limit,
+        #    accel_limit=accel_limit, wheel_radius=wheel_radius, wheel_base=wheel_base, steer_ratio=steer_ratio,
+        #    max_lat_accel=max_lat_accel, max_steer_angle=max_steer_angle)
         
-
+        self.controller = Controller(
+            wheel_base=wheel_base, steer_ratio=steer_ratio,  
+            max_lat_accel=max_lat_accel, max_steer_angle=max_steer_angle,
+            accel_limit=accel_limit, decel_limit=decel_limit,
+            vehicle_mass=vehicle_mass, fuel_capacity=fuel_capacity,
+            brake_deadband=brake_deadband, wheel_radius=wheel_radius)
+            
         # TODO: Subscribe to all the topics you need to
         rospy.subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)
         rospy.subscriber('/twist_cmd', TwistStamped, self.twist_cb)
